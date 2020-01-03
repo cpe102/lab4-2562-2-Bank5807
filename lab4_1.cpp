e3d83363792fb7ace7930eebf2b6ac8aebc9eb66
Bank5807 <tanat_wip@cmu.ac.th>
2020-01-03 15:32:12 +0700

    lab4_1

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double paraEuler(int x)
+    {
+        return (3*pow(x,3)) + (2*2.718281) + (pow(2,((2*x)+1))) + sqrt(pow(x,2)+1);
+    }
+
+int main()
+    {
+        int x;
+        cout << "Enter x = ";
+        cin >> x;
+        cout << "Result y = " << paraEuler(x);
+        return 0;
+    }
 
