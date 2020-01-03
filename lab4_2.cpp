28e6096601da854c3d924b468bf0657986a019b3
Bank5807 <tanat_wip@cmu.ac.th>
2020-01-03 15:39:28 +0700

    lab4_2 v1

--- lab4_2.cpp
+++ lab4_2.cpp
@@ -1,12 +1,16 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
 //Write function findDistance() here
-
-int main(){
-
-  //Calling findDistance() using test cases
-
-  return 0;
+double findDistance(double u, double a, double t)
+{
+    return (u*t) + (0.5*(a*t*t));
 }
+
+int main()
+{
+  //Calling findDistance() using test cases
+    cout << findDistance(0,0.5,1) << endl;
+    return 0;
+}
+
